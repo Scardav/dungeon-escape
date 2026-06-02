8569cf6414a21be0ad348586759b52ad78621793
DAVI BOLSONI <davi_bolsoni@estudante.sesisenai.org.br>
2026-06-02 17:17:51 -0300

    Inimigo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 
 int main() {
 	
-		Personagem heroi("Herói", 100, 10);
+	Personagem heroi("Herói", 100, 10);
 	heroi.apresentar();
+	
+	Inimigo goblin("Goblin", 30, 5, "Goblin");
+	goblin.apresentar();
+
 	return 0;
 }
--- personagem.h
+++ personagem.h
@@ -14,6 +14,21 @@ public:
 	void apresentar(){
 		std::cout << "Personagem: " << nome   << std::endl;
 		std::cout << "vida"         << vida   << std::endl;
-		std::cout << "Ataque: "    << ataque << std::endl;
+		std::cout << "Ataque: "     << ataque << std::endl;
 	}
+	
 };
+
+class Inimigo : public Personagem {
+public:
+	std::string tipo;
+	
+		Inimigo(std::string n, int v, int a, std::string t)
+		: Personagem(n, v, a), tipo(t) {}
+						
+		void apresentar(){
+				std::cout << "[INIMIGO] "   << nome   << std::endl;
+				std::cout << " | Tipo: "       << nome   << std::endl;
+				std::cout << " | Vida: "       << vida   << std::endl;
+				std::cout << " | Ataque: "     << ataque << std::endl;
+}
